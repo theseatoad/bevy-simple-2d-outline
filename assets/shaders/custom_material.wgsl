5485d86d63a1ae98e4f81076986e57c1331d5615
TheSeaToad <me@theseatoad.com>
2022-11-30 18:40:18 -0800

    Example using a wgsl file

---
+++ assets/shaders/custom_material.wgsl
@@ -0,0 +1,28 @@
+struct CustomMaterial {
+    color: vec4<f32>,
+};
+@group(1) @binding(0)
+var<uniform> material: CustomMaterial;
+@group(1) @binding(1)
+var base_color_texture: texture_2d<f32>;
+@group(1) @binding(2)
+var base_color_sampler: sampler;
+
+@fragment
+fn fragment(
+    #import bevy_pbr::mesh_vertex_output
+    @builtin(position) coord : vec4<f32>
+) -> @location(0) vec4<f32> {    
+    var outline : f32 = textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(0.01,0.0)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(-0.01,0.0)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(0.0,0.01)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(0.0,-0.01)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(0.01,-0.01)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(-0.01,0.01)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(0.01,0.01)).a;
+    outline += textureSample(base_color_texture, base_color_sampler,uv + vec2<f32>(-0.01,-0.01)).a;
+    outline = min(outline, 1.0);
+    var color : vec4<f32> = textureSample(base_color_texture, base_color_sampler,uv);
+    return mix(color, material.color, outline - color.a);
+    //return material.color * textureSample(base_color_texture, base_color_sampler,uv);
+}
---
+++ examples/custom-material.rs
@@ -0,0 +1,56 @@
+//! A shader and a material that uses it.
+
+use bevy::{
+    asset::{AssetServer, Assets},
+    prelude::*,
+    reflect::TypeUuid,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+    utils::default,
+};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(Material2dPlugin::<CustomMaterial>::default())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(CustomMaterial {
+            color: Color::BLUE,
+            color_texture: Some(asset_server.load("textures/sprite_seatoad.png")),
+        }),
+        ..default()
+    });
+
+    commands.spawn(Camera2dBundle::default());
+}
+
+/// The Material trait is very configurable, but comes with sensible defaults for all methods.
+/// You only need to implement functions for features that need non-default behavior. See the Material api docs for details!
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material.wgsl".into()
+    }
+}
+
+// This is the struct that will be passed to your shader
+#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
+#[uuid = "f690fdae-d598-45ab-8225-97e2a3f056e0"]
+pub struct CustomMaterial {
+    #[uniform(0)]
+    pub color: Color,
+    #[texture(1)]
+    #[sampler(2)]
+    pub color_texture: Option<Handle<Image>>,
+}
